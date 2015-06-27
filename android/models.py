72fd06e6b2ba9fdefc3475dca38d17d26768344e
Omkar Jadhav <omkarjadhav003@gmail.com>
2015-06-27 12:58:18 +0530

    added g+ sign and authentication for msgs

--- android/models.py
+++ android/models.py
@@ -3,11 +3,16 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
 	name=models.CharField(max_length=100,null=True,blank=True)
-	token=models.CharField(unique=True,max_length=1000)
+	token=models.CharField(max_length=1000)
+	email=models.CharField(unique=True,max_length=100)
+	phone_no=models.CharField(max_length=20,null=True,blank=True)
+	image_url=models.CharField(max_length=100,null=True,blank=True)
+	google_id=models.CharField(max_length=100)
+	role=models.CharField(max_length=100,null=True,blank=True)
 	def __str__(self):
-		return "%s" % (self.name)
+		return "%s %s" % (self.name,self.email)
 	def __unicode__(self):
-		return "%s" % (self.name)
+		return "%s %s" % (self.name,self.email)
 	
 class Noti(models.Model):
 	api_key=models.CharField(max_length=1000)
